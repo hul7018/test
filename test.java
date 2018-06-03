bc699af9c1064d01f25906643bc2d4c53db62995
hul7018 <39864850+hul7018@users.noreply.github.com>
2018-06-03 19:56:35 +0200

    修改java文件
    第一次使用github，练习一把

--- test.java
+++ test.java
@@ -1,5 +1,7 @@
 public class Test{
     public static void main(String[] args){
-        System.out.println("Hello world");    
+        for (int i = 0; i < 5; i++){   
+            System.out.println("Hello world");    
+        }
     }
 }
